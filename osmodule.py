bf466c7d1ba4015ac425ccb774d8072d7514d99c
prashanthkolukuluri2248 <146011367+prashanthkolukuluri2248@users.noreply.github.com>
2023-12-07 09:26:15 +0530

    Delete osmodule.py

--- osmodule.py
+++
@@ -1,9 +0,0 @@
-import os
-print(os.getcwd())
-def list_files_in_current_directory():
-    current_dir = os.getcwd()
-    files = [file for file in os.listdir(current_dir) if os.path.isfile(os.path.join(current_dir, file))]
-    print("Files in the current directory:")
-    for file_name in files:
-        print(file_name)
-list_files_in_current_directory()
